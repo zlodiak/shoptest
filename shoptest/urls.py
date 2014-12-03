10f7724899e95d8e011d66058ed3a567acd13d60
Your Name <you@example.com>
2014-12-03 19:21:29 +0400

    shop ok

--- shoptest/urls.py
+++ shoptest/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'shoptest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^shop/', include('shop.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
