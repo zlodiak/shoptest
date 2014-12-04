40c9f5b4973f474517ba96ad21ecf79c8e3943f3
Your Name <you@example.com>
2014-12-04 19:23:06 +0400

    werwrwer erwrwerwer werwerwer fdgdfgd 2423

--- shoptest/settings.py
+++ shoptest/settings.py
@@ -13,7 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
+# №Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
